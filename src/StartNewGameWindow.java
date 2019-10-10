d950439386d9b803d61dfe025d51721afabbf858
NickBodmer <bodmerr@yandex.ru>
2019-10-10 16:02:20 +0300

    начало

--- src/StartNewGameWindow.java
+++ src/StartNewGameWindow.java
@@ -11,12 +11,32 @@ public class StartNewGameWindow extends JFrame {
     private static final int MAX_WIN_LEN = 10;
     private static final String STR_WIN_LEN = "Winning Lenght";
     private static final String STR_FIELD_SIZE = "Field Size";
-
+    private JRadioButton jrbHumanvsAI = new JRadioButton("Human vs AI", true);
+    private JRadioButton jrbHumanvsHuman = new JRadioButton("Human vs Human");
+    private ButtonGroup gameMode = new ButtonGroup();
+    private JSlider slFieldSize;
+    private JSlider slWinLenght;
 
     public StartNewGameWindow(GameWindow gameWindow) {
         this.gameWindow = gameWindow;
         setVisible(false);
+        setSize(WIN_WIDHT, WIN_HEIGHT);
+        setTitle("Game option");
+        Rectangle gameWindowsBounds = gameWindow.getBounds();
+        int posX = (int) gameWindowsBounds.getCenterX() - WIN_WIDHT / 2;
+        int posY = (int) gameWindowsBounds.getCenterX() - WIN_HEIGHT / 2;
+        setLocation(posX, posY);
+        setLayout(new GridLayout(10, 1));
+        addGameControlMode();
+    }
 
+
+    private void addGameControlMode() {
+        add(new Label("Choose game mode"));
+        gameMode.add(jrbHumanvsAI);
+        gameMode.add(jrbHumanvsHuman);
+        add(jrbHumanvsAI);
+        add(jrbHumanvsHuman);
     }
 
 }
